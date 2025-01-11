0b64379f37da631d8ef2393112360c18174306d7
zjwqsd <221502006@smail.nju.edu.cn>
2025-01-11 16:49:41 +0800

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import preact from '@astrojs/preact';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+// import content from 'astro-content';
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zjwqsd.github.io',
+  site: 'https://zjwqsd.github.io',
+  vite: { optimizeDeps: { include: ['asciinema-player'] } },
+  markdown: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+  },
+  integrations: [preact(), mdx(), react()],
 });
---
+++ src/pages/posts/test.md
@@ -0,0 +1,4 @@
+---
+title: '123'
+description: '这是我 Astro 博客的第一篇文章。'
+---
